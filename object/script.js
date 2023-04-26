aa8be98ef5dc104ebb5b66355757f1e2eba74abb
Nurgul Yunusova <nurgulyunusova03@gmail.com>
2023-04-27 00:42:42 +0400

    Completed object task

--- object/script.js
+++ object/script.js
@@ -104,31 +104,73 @@
 //   ["Ben"]
 // ); // Killer Megan
 
-// TASK 5
-function getWinner(applicants, winnerObject) {}
+// // TASK 5
+// function getWinner(applicants, winnerObject) {
+//   let applicantsKeys = Object.keys(applicants);
+//   let randomIndex = getRandomNumberInRange(0, applicantsKeys.length);
+//   let winnerKey = applicantsKeys[randomIndex];
+//   let winner = applicants[winnerKey];
+//   return Object.assign({}, winnerObject, winner);
+// }
 
-function getRandomNumberInRange(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
-}
+// function getRandomNumberInRange(min, max) {
+//   return Math.floor(Math.random() * (max - min)) + min;
+// }
 
-const todaysWinner = {
-  prize: "$10,000",
-};
+// const todaysWinner = {
+//   prize: "$10,000",
+// };
 
-const winnerApplicants = {
-  "001": {
-    name: "Max",
-    age: 25,
-  },
-  201: {
-    name: "Svetlana",
-    age: 20,
-  },
-  304: {
-    name: "Catherine",
-    age: 35,
-  },
-};
+// const winnerApplicants = {
+//   "001": {
+//     name: "Max",
+//     age: 25,
+//   },
+//   201: {
+//     name: "Svetlana",
+//     age: 20,
+//   },
+//   304: {
+//     name: "Catherine",
+//     age: 35,
+//   },
+// };
 
-const resultWinner = getWinner(winnerApplicants, todaysWinner);
-console.log("resultWinner", resultWinner); // { prize: '$10,000', name: 'Maxim', age: 25 }
+// const resultWinner = getWinner(winnerApplicants, todaysWinner);
+// console.log("resultWinner", resultWinner); // { prize: '$10,000', name: 'Maxim', age: 25 }
+
+// // TASK 6
+// function giveTalonsInOrder(patients, orders) {
+//   let result = [];
+
+//   for (let i = 0; i < orders.length; i++) {
+//     let order = orders[i];
+//     let patient = patients.find((p) => p.id === order);
+
+//     if (patient) {
+//       result.push(patient);
+//     }
+//   }
+
+//   return result;
+// }
+
+// const ordersArr = [4, 2, 1, 3];
+// const people = [
+//   { id: 1, name: "Maxim" },
+//   { id: 2, name: "Nicholas" },
+//   { id: 3, name: "Angelina" },
+//   { id: 4, name: "Vitaly" },
+// ];
+
+// const result = giveTalonsInOrder(people, ordersArr);
+
+// console.log("result", result);
+// /* Returns an array
+// [
+//     { id: 4, name: 'Vitaly' },
+//     { id: 2, name: 'Nicholas' },
+//     { id: 1, name: 'Maxim' },
+//     { id: 3, name: 'Angelina' }
+// ]
+// */
